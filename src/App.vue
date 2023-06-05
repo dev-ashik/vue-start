9df51b901a8f618ec527cdbf29486d46dec7ef12
MD ASHIK MAHMUD <61373940+dev-ashik@users.noreply.github.com>
2023-06-05 09:45:40 +0800

    canged

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div>
+      <h4>The courrent count is ...</h4>
+      <h1>{{ count }}</h1>
+      <div class="button-div">
+        <button @click="$event => count--">-</button>
+        <button @click="$event => count++">+</button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  const count = ref(0);
+
+</script>
+
+<style scoped>
+  
+
+  main {
+    background-color: aliceblue;
+    height: 100vh;
+    width: 100vw;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+
+  h4 {
+    font-size: 2rem;
+  }
+  .button-div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+  }
+
+  .button-div button {
+    font-size: 2rem;
+    height: 40px;
+    width: 40px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
