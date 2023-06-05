2a1bf334c0ff4c3de865f8ac8b7da158e4ee07d5
MD ASHIK MAHMUD <61373940+dev-ashik@users.noreply.github.com>
2023-06-05 10:48:11 +0800

    Composition API count

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,81 @@
+<!-- <script setup>
+  // option API
+import { ref } from "vue";
+const count = ref(0);
+
+const addToCount = () => {
+  count.value = count.value + 1
+}
+const subtractFromCout = () => {
+  count.value = count.value - 1
+}
+</script> -->
+
+<script>
+// Composition API
+
+export default ({
+  data(){
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addToCount() {
+      this.count = this.count + 1;
+    },
+    subtractFromCout(){
+     this.count = this.count - 1;
+    }
+  }
+})
+</script>
+
 <template>
   <main>
     <div>
       <h4>The courrent count is ...</h4>
       <h1>{{ count }}</h1>
       <div class="button-div">
-        <button @click="$event => count--">-</button>
-        <button @click="$event => count++">+</button>
+        <button @click="subtractFromCout">-</button>
+        <button @click="addToCount">+</button>
       </div>
     </div>
   </main>
 </template>
 
-<script setup>
-  import { ref } from 'vue';
-  const count = ref(0);
-
-</script>
-
 <style scoped>
-  
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
 
-  main {
-    background-color: aliceblue;
-    height: 100vh;
-    width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+div {
+  text-align: center;
+}
 
-  div {
-    text-align: center;
-  }
+h4 {
+  font-size: 2rem;
+}
+.button-div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
 
-  h4 {
-    font-size: 2rem;
-  }
-  .button-div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 1rem;
-  }
+.button-div button {
+  font-size: 2rem;
+  height: 40px;
+  width: 40px;
 
-  .button-div button {
-    font-size: 2rem;
-    height: 40px;
-    width: 40px;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
